cd38271f9937bd432275422116306592d903ca98
masashi tominaga <m.tominaga@leihauoli.com>
2016-12-26 10:50:09 +0900

    livereloadを使用するようにconfigを戻す、gem fileに記入

--- config.rb
+++ config.rb
@@ -16,6 +16,13 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
 ###
 # Helpers
 ###
