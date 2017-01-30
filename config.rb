4ccff7e4bf19224f192c0dbc3c9ed47a8dfee84e
masashi tominaga <m.tominaga@leihauoli.com>
2017-01-30 16:48:32 +0900

    ブログ機能の設定を修正

--- config.rb
+++ config.rb
@@ -26,27 +26,27 @@ page '/*.txt', layout: false
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  blog.prefix = "article"
+  # blog.prefix = "articles"
 
-  blog.permalink = "{category}/{title}.html"
+  blog.permalink = "{url}/index.html"
   # Matcher for blog source files
-  blog.sources = "{year}{month}{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.sources = "articles/{year}/{month}/{day}-{title}.html"
+  blog.taglink = "categories/{tag}.html"
+  blog.layout = "article_layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
