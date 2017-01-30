8cae2d4e50417bf817c65837f2d9379c99f28ebe
masashi tominaga <m.tominaga@leihauoli.com>
2017-01-30 16:46:32 +0900

    ラブリロード機能の場所を変更

--- config.rb
+++ config.rb
@@ -20,11 +20,6 @@ page '/*.txt', layout: false
 
 # General configuration
 
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
 ###
 # Helpers
 ###
@@ -56,9 +51,9 @@ end
 
 page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
