b75bcf1533ddf7a97fec7898413a282309f91bd6
masashi tominaga <m.tominaga@leihauoli.com>
2017-03-19 01:38:11 +0900

    delete header menu button

--- source/articles/javascripts/common.js
+++ source/articles/javascripts/common.js
@@ -9,26 +9,6 @@ MOYASHI.LINK = {
   }
 }
 
-MOYASHI.HUMBERGER = {
-  init : function() {
-    this.setParameters();
-    this.bindEvent();
-  },
-  setParameters : function() {
-    this.$trigger = $('.js-menu-btn');
-    this.$header = $('.js-header-list');
-  },
-  bindEvent : function() {
-    var self = this;
-    this.$trigger.on('click',function(e) {
-      e.preventDefault();
-      $(this).toggleClass('active');
-      self.$header.fadeToggle(100);
-    })
-  }
-}
-
 $(function(){
   MOYASHI.LINK.init();
-  MOYASHI.HUMBERGER.init();
 })
